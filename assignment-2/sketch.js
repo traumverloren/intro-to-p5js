7f8f44d2313cb58376e49783e40df83e4e54bdd7
Stephanie Nemeth <traumverloren@gmail.com>
2017-05-31 20:19:40 +0200

    playing around with loops and frameRate

--- assignment-2/sketch.js
+++ assignment-2/sketch.js
@@ -1,7 +1,35 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(255);
+  noFill();
+  frameRate(5);
 }
 
 function draw() {
+  background(255);
+
+  if (frameCount % 1 === 0) {
+    var randomColor = random(10,255);
+
+    for (var x = 50; x <= width-50; x += 50) {
+      for (var y = 50; y <= height-50; y += 50) {
+        fill(randomColor);
+        ellipse(x, y, 30, 30);
+        noFill();
+      }
+    }
+  }
+
+  var count = 0;
+  for (var x = 50; x <= width-50; x += 50) {
+    for (var y = 50; y <= height-50; y += 50) {
+      stroke(count);
+      var randX = random(-x, x);
+      var randY = random(-y, y);
+      line(randX, randY, 0, 0);
+      count++;
+    }
+  }
+
+  // fill(255);
+  // ellipse(width/2, height/2, 100, 100);
 }
