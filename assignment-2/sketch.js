4b2e5fa55275c3c602358a8d89c87d4b5bf7940f
Stephanie Nemeth <traumverloren@gmail.com>
2017-05-31 20:42:38 +0200

    playing around with frameCount

--- assignment-2/sketch.js
+++ assignment-2/sketch.js
@@ -1,34 +1,37 @@
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noFill();
-  frameRate(5);
+  frameRate(30);
 }
 
 function draw() {
   background(255);
 
+  var count = frameCount;
+
   if (frameCount % 1 === 0) {
     var randomColor = random(10,255);
-
     for (var x = 50; x <= width-50; x += 50) {
+      strokeWeight(0);
+      fill(count);
       for (var y = 50; y <= height-50; y += 50) {
-        fill(randomColor);
-        ellipse(x, y, 30, 30);
-        noFill();
+        ellipse(x, y, 50, 50);
       }
+      noFill();
     }
   }
 
-  var count = 0;
-  for (var x = 50; x <= width-50; x += 50) {
-    for (var y = 50; y <= height-50; y += 50) {
-      stroke(count);
-      var randX = random(-x, x);
-      var randY = random(-y, y);
-      line(randX, randY, 0, 0);
-      count++;
-    }
-  }
+
+  console.log(count)
+
+  //
+  // for (var x = 50; x <= width-50; x += 50) {
+  //   for (var y = 50; y <= height-50; y += 50) {
+  //     var randX = random(-x, x);
+  //     var randY = random(-y, y);
+  //     ellipse(randX, randY, 20, 20);
+  //   }
+  // }
 
   // fill(255);
   // ellipse(width/2, height/2, 100, 100);
