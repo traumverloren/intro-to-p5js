1bfc8efa473ff75355315b0fcdc5ff61c05acfd7
Stephanie Nemeth <traumverloren@gmail.com>
2017-06-01 20:49:46 +0200

    still issues, but all 4 quadrants now

--- assignment-2/sketch.js
+++ assignment-2/sketch.js
@@ -1,28 +1,47 @@
+var framesPerSecond = 256;
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
   noFill();
-  frameRate(30);
+  frameRate(framesPerSecond);
 }
 
 function draw() {
   background(255);
 
-  var count = frameCount;
+  var gray = frameCount % 128;
 
-  if (frameCount % 1 === 0) {
-    var randomColor = random(10,255);
-    for (var x = 50; x <= width-50; x += 50) {
-      strokeWeight(0);
-      fill(count);
-      for (var y = 50; y <= height-50; y += 50) {
-        ellipse(x, y, 50, 50);
-      }
-      noFill();
+  // top right quadrant
+  for (var x = 50; x <= (windowWidth-50); x += 50) {
+    noStroke();
+    fill(gray);
+    for (var y = 50; y <= (windowHeight/2)-50; y += 50) {
+      triangle(x, y-20, x+20, y+20, x-20, y);
     }
+    noFill();
   }
 
+  // bottom left quadrant
+  for (var x = 50; x <= (windowWidth/2)-50; x += 50) {
+    stroke(gray);
+    strokeWeight(10);
+    for (var y = 50; y <= (windowHeight-50); y += 50) {
+      line(x-7, y-7, x+7, y+7);
+      line(x+7, y-7, x-7, y+7);
+    }
+    noFill();
+  }
 
-  console.log(count)
+  // bottom right quadrant
+  for (var x = (windowWidth/2); x <= (windowWidth-50); x += 50) {
+    strokeWeight(0);
+    fill(gray);
+    for (var y = windowHeight/2; y <= (windowHeight-50); y += 50) {
+      rect(x-35, y-20, 25, 25);
+      console.log(x,y);
+    }
+    noFill();
+  }
 
   //
   // for (var x = 50; x <= width-50; x += 50) {
