ac5cbb7ef1221b42c081e03918709182cfffb4bb
Stephanie Nemeth <traumverloren@gmail.com>
2017-06-03 23:01:22 +0200

    fucking awesome using p5sound

--- assignment-2/sketch.js
+++ assignment-2/sketch.js
@@ -1,77 +1,51 @@
 var angle = 0;
-var a;
-var b;
-var direction;
+var song;
+var amplitude;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   colorMode(RGB, 255);
   noFill();
-  frameRate(60)
-  a = 0;
-  b = width;
-  direction = true;
+
+  // setting up the sound
+  song = loadSound('audio/06 Don\'t Talk (Redux).mp3', playSound);
+  song.setVolume(0.2);
+  amplitude = new p5.Amplitude();
+}
+
+// play song callback
+function playSound() {
+  console.log("Playing song!");
+  song.play();
 }
 
 function draw() {
+  var level = amplitude.getLevel();
+  var size = map(level, 0, 1, 0, 200);
+
   // math to make the shapes fade black to white
   angle += 0.02;
   var grayscaleTransition = 127+127*sin(angle);
 
-  // var count = 0;
-  // for (var x = 50; x <= (windowWidth-50); x += 50) {
-  //   for (var y = 50; y <= (windowHeight-50); y += 50) {
-  //     stroke(240)
-  //     strokeWeight(count+0.02);
-  //     point(x,y)
-  //     console.log(count)
-  //
-  //     count++;
-  //   }
-  // }
-
-  // fading background
-  a++;
-  if(a > width) {
-    a = 0;
-    direction = !direction;
-  }
-  if(direction == true){
-    stroke(a, 173);
-  } else {
-    stroke(width-a, 173);
-  }
-  line(a, 0, a, height);
-
-  b--;
-  if(b < 0) {
-    b = width;
-  }
-  if(direction == true) {
-    stroke(width-b, 127);
-  } else {
-    stroke(b, 127);
-  }
-  line(b, 0, b, height);
-
-  // the 4 quadrant shapes:
+  // the 4 quadrants:
 
   // top left quadrant
+  background(255)
+  stroke(0);
+  strokeWeight(1);
+  fill(grayscaleTransition);
   for (var x = 50; x <= (windowWidth/2)-50; x += 50) {
-    noStroke();
-    fill(grayscaleTransition);
     for (var y = 50; y <= (windowHeight/2)-50; y += 50) {
-      ellipse(x, y, 25, 25);
+      ellipse(x, y, size, size);
     }
-    // noFill();
   }
 
   // top right quadrant
+  stroke(0);
+  strokeWeight(1);
   for (var x = (windowWidth/2)-30; x <= (windowWidth-80); x += 50) {
-    noStroke();
-    fill(grayscaleTransition);
     for (var y = 50; y <= (windowHeight/2)-50; y += 50) {
-      triangle(x, y-10, x+20, y+5, x, y+20);
+      triangle(x, y-size, x+size, y+5, x, y+size);
     }
   }
 
@@ -80,17 +54,17 @@ function draw() {
     stroke(grayscaleTransition);
     strokeWeight(10);
     for (var y = (windowHeight/2); y <= (windowHeight-50); y += 50) {
-      line(x-7, y-7, x+7, y+7);
-      line(x+7, y-7, x-7, y+7);
+      line(x-size, y-size, x+size, y+size);
+      line(x+size, y-size, x-size, y+size);
     }
   }
 
   // bottom right quadrant
+  stroke(0);
+  strokeWeight(1);
   for (var x = (windowWidth/2); x <= (windowWidth-50); x += 50) {
-    fill(grayscaleTransition);
     for (var y = (windowHeight/2)+10; y <= (windowHeight-40); y += 50) {
-      rect(x-35, y-17, 15, 15);
+      rect(x-35, y-17, size, size);
     }
   }
-
 }
